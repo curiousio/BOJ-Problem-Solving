238351dfbc8d56b382c29fc53120a8f1caeb7b3f
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-07-20 01:02:20 +0900

    Create 2444.c
    별 찍기 - 7

---
+++ 2444.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i + j > n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        for (; j <= i + n - 1; j++)
+        {
+            if (i + j > n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i + j > n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        for (; j <= i + n - 1; j++)
+        {
+            if (i + j > n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
