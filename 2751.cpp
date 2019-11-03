62b23b75d722daa541af05b0eebba89f3d80d013
miniesam <46525418+miniesam@users.noreply.github.com>
2019-11-03 15:30:25 +0900

    Create 2751.cpp
    수 정렬하기 2

---
+++ 2751.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+int n;
+int a[1000000];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", a + i);
+    }
+
+    std::sort(a, a + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", *(a + i));
+    }
+    
+    return 0;
+}
