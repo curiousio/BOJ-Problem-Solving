958db36cdcc4e4b8bc8288f8e340525cf0a5a1a9
miniesam <46525418+miniesam@users.noreply.github.com>
2019-11-03 15:26:55 +0900

    Create 11727.c
    2×n 타일링 2

---
+++ 11727.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int DP[1001] = { 0, 1, 3 };
+
+    scanf("%d", &n);
+
+    for (int i = 3; i <= n; i++)
+    {
+        DP[i] = (DP[i - 1] + DP[i - 2] * 2) % 10007;
+    }
+
+    printf("%d", DP[n]);
+
+    return 0;
+}
