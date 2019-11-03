4ad45565f0d6520807b4a0e1debbb70a6d1e09b2
miniesam <46525418+miniesam@users.noreply.github.com>
2019-11-03 15:38:14 +0900

    Create 2747.c
    피보나치 수

---
+++ 2747.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int fibo[50] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 };
+
+    scanf("%d", &n);
+
+    for (int i = 18; i <= n; i++)
+    {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+
+    printf("%d", fibo[n]);
+
+    return 0;
+}
