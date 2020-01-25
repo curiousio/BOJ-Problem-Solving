997c48dae364b8a2e1d47234819109e99b2b2ed7
miniesam <46525418+miniesam@users.noreply.github.com>
2020-01-25 20:03:47 +0900

    Create 2161.cpp
    카드1

---
+++ 2161.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <cstdio>
+
+int main()
+{
+    int n;
+    std::queue <int> q;
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        q.push(i);
+    }
+
+    while (n-- >= 2)
+    {
+        printf("%d ", q.front());
+        q.pop();
+        int temp;
+        temp = q.front();
+        q.pop();
+        q.push(temp);
+    }
+
+    printf("%d", q.front());
+
+    return 0;
+}
